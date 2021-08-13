3b37d95ad625691b500c611bbfdefd5f8a76a255
Emanuelle Keller <87922139+Eukeller@users.noreply.github.com>
2021-08-13 17:08:03 -0300

    Add files via upload
    Coloquei como FLOAT para testar e acredito que tenha funcionado bem :)

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+#*Faça um Programa que peça dois números e imprima a soma.
+nmr = float(input("Digite um número:"))
+nmr2 = float(input("Digite mais um número:"))
+
+soma = (nmr + nmr2)
+
+total = print("A soma dos números é igual a:",soma)
---
+++ exercicio2.py
@@ -0,0 +1,10 @@
+#*Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+nt1 = int(input("Primeiro bimestre: "))
+nt2 = int(input("Segundo bimestre: "))
+nt3 = int(input("Terceiro  bimestre: "))
+nt4 = int(input("Quarto bimestre: "))
+
+somas = (nt1 + nt2 + nt3 + nt4)/4
+
+print("A média dos bimestres é igual a: ", somas)
+
